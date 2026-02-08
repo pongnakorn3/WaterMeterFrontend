ee7aebe3da23bae88e55e1600b2a971083def762
pongnakorn3 <pongnakorn3@gmail.com>
2026-02-08 20:24:24 +0700

    Connect API

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://watermeterbackend-production.up.railway.app';
 
 function Login({ onLoginSuccess }) {
   const [username, setUsername] = useState('');
